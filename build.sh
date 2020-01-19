3b4496b0d5db71cb4994e4b761153e48ad650546
Vitaliy Yushkevich <yushkevichv@gmail.com>
2020-01-20 00:40:24 +0300

    add second slave

--- build.sh
+++ build.sh
@@ -25,21 +25,35 @@ done
 
 echo "connection to slave is ok"
 
+until docker exec mysql_slave2 mysql -uroot -ppwd -e ";"
+do
+    echo "Waiting for mysql_slave2 database connection..."
+    sleep 4
+done
+
+echo "connection to slave2 is ok"
+
 
 docker-ip() {
     docker inspect --format '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "$@"
 }
 
 MS_STATUS=`docker exec mysql_master  mysql -uroot -ppwd -e "SHOW MASTER STATUS"`
-echo $MS_STATUS
-echo $MS_STATUS | awk '{print $6}'
 CURRENT_LOG=`echo $MS_STATUS | awk '{print $6}'`
 CURRENT_POS=`echo $MS_STATUS | awk '{print $7}'`
 
-start_slave_stmt="CHANGE MASTER TO MASTER_HOST='$(docker-ip mysql_master)',MASTER_USER='mydb_slave_user',MASTER_PASSWORD='mydb_slave_pwd',MASTER_LOG_FILE='$CURRENT_LOG',MASTER_LOG_POS=$CURRENT_POS; START SLAVE;"
-start_slave_cmd='export MYSQL_PWD=111; mysql -u root -e "'
-start_slave_cmd+="$start_slave_stmt"
-start_slave_cmd+='"'
+start_slave_stmt="CHANGE MASTER TO MASTER_HOST='$(docker-ip mysql_master)',MASTER_USER='slave_user',MASTER_PASSWORD='slave_pwd',MASTER_LOG_FILE='$CURRENT_LOG',MASTER_LOG_POS=$CURRENT_POS; START SLAVE;"
 
 docker exec mysql_slave mysql -uroot -ppwd -e "$start_slave_stmt"
 docker exec mysql_slave mysql -uroot -ppwd -e 'SHOW SLAVE STATUS \G'
+
+echo "and for second slave"
+
+MS_STATUS=`docker exec mysql_master  mysql -uroot -ppwd -e "SHOW MASTER STATUS"`
+CURRENT_LOG=`echo $MS_STATUS | awk '{print $6}'`
+CURRENT_POS=`echo $MS_STATUS | awk '{print $7}'`
+
+start_slave_stmt="CHANGE MASTER TO MASTER_HOST='$(docker-ip mysql_master)',MASTER_USER='slave_user',MASTER_PASSWORD='slave_pwd',MASTER_LOG_FILE='$CURRENT_LOG',MASTER_LOG_POS=$CURRENT_POS; START SLAVE;"
+
+docker exec mysql_slave2 mysql -uroot -ppwd -e "$start_slave_stmt"
+docker exec mysql_slave2 mysql -uroot -ppwd -e 'SHOW SLAVE STATUS \G'
