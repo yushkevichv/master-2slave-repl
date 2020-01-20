4b6e3e6418e3e6ae9bc9abf3c7816da7985c05a2
Vitaliy Yushkevich <yushkevichv@gmail.com>
2020-01-20 13:38:08 +0300

    cleanup

--- build.sh
+++ build.sh
@@ -49,11 +49,7 @@ docker exec mysql_slave mysql -uroot -ppwd -e 'SHOW SLAVE STATUS \G'
 
 echo "and for second slave"
 
-MS_STATUS=`docker exec mysql_master  mysql -uroot -ppwd -e "SHOW MASTER STATUS"`
-CURRENT_LOG=`echo $MS_STATUS | awk '{print $6}'`
-CURRENT_POS=`echo $MS_STATUS | awk '{print $7}'`
-
-start_slave_stmt="CHANGE MASTER TO MASTER_HOST='$(docker-ip mysql_master)',MASTER_USER='slave_user',MASTER_PASSWORD='slave_pwd',MASTER_LOG_FILE='$CURRENT_LOG',MASTER_LOG_POS=$CURRENT_POS; START SLAVE;"
-
 docker exec mysql_slave2 mysql -uroot -ppwd -e "$start_slave_stmt"
 docker exec mysql_slave2 mysql -uroot -ppwd -e 'SHOW SLAVE STATUS \G'
+
+#CHANGE MASTER TO MASTER_HOST='192.168.192.0',MASTER_USER='slave_user',MASTER_PASSWORD='slave_pwd',MASTER_LOG_FILE='mysql-bin.000003',MASTER_LOG_POS=595; START SLAVE;
