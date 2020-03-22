01cba161ee65b34b76565cca4d88b3c5d85f8249
Vitaliy Yushkevich <yushkevichv@gmail.com>
2020-03-22 18:22:36 +0300

    change work to one replica; add faker to generate human data for next operation.

--- app/index.php
+++ app/index.php
@@ -1,15 +1,20 @@
 <?php
-$redis = new Redis();
+require_once 'vendor/autoload.php';
 
-try {
-    $redis->connect('redis');
-}
-catch (\Exception $e)
-{
-    die($e->getMessage());
-}
+$faker = Faker\Factory::create('ru_RU');
 
-$redis->set('counter', 0);
+
+//$redis = new Redis();
+//
+//try {
+//    $redis->connect('redis');
+//}
+//catch (\Exception $e)
+//{
+//    die($e->getMessage());
+//}
+//
+//$redis->set('counter', 0);
 
 
 $mysqli = new mysqli("mysql_master", "user", "pwd", "test_repl_db");
@@ -19,28 +24,31 @@ if ($mysqli->connect_errno) {
 $mysqli->set_charset('utf8');
 
 
-if (!($stmt = $mysqli->prepare("INSERT INTO test(id, name) VALUES (null, ?)"))) {
+if (!($stmt = $mysqli->prepare("INSERT INTO users(id, name) VALUES (null, ?)"))) {
     echo "Не удалось подготовить запрос: (" . $mysqli->errno . ") " . $mysqli->error;
 }
 
-$id = 1;
-if (!$stmt->bind_param("i", $id)) {
-    echo "Не удалось привязать параметры: (" . $stmt->errno . ") " . $stmt->error;
-}
+//$id = 1;
+//if (!$stmt->bind_param("i", $id)) {
+//    echo "Не удалось привязать параметры: (" . $stmt->errno . ") " . $stmt->error;
+//}
 
-for ($id = 1; $id <= 1000000; $id++) {
+for ($id = 1; $id <= 10000; $id++) {
+    if (!$stmt->bind_param("s", $faker->name)) {
+        echo "Не удалось привязать параметры: (" . $stmt->errno . ") " . $stmt->error;
+    }
     if (!$stmt->execute()) {
         echo "Не удалось выполнить запрос: (" . $stmt->errno . ") " . $stmt->error;
-        usleep(100);
+//        usleep(100);
     }
-    else {
-        $redis->incr('counter');
-        continue;
-    }
+//    else {
+//        $redis->incr('counter');
+//        continue;
+//    }
 }
 
 echo "Запрос успешно выполнен \r\n";
 
 $stmt->close();
 
-echo "Количество успешно добавленных записей: ". (int) $redis->get('counter') ."\r\n";
+//echo "Количество успешно добавленных записей: ". (int) $redis->get('counter') ."\r\n";
--- build.sh
+++ build.sh
@@ -28,13 +28,13 @@ done
 
 echo "connection to slave is ok"
 
-until docker exec mysql_slave2 mysql -uroot -ppwd -e ";"
-do
-    echo "Waiting for mysql_slave2 database connection..."
-    sleep 4
-done
+#until docker exec mysql_slave2 mysql -uroot -ppwd -e ";"
+#do
+#    echo "Waiting for mysql_slave2 database connection..."
+#    sleep 4
+#done
 
-echo "connection to slave2 is ok"
+#echo "connection to slave2 is ok"
 
 MS_STATUS=`docker exec mysql_master  mysql -uroot -ppwd -e "SHOW MASTER STATUS"`
 CURRENT_LOG=`echo $MS_STATUS | awk '{print $6}'`
@@ -50,18 +50,18 @@ docker exec mysql_slave mysql -uroot -ppwd -e "INSTALL PLUGIN rpl_semi_sync_slav
 docker exec mysql_slave mysql -uroot -ppwd -e "$start_slave_stmt"
 docker exec mysql_slave2 mysql -uroot -ppwd -e 'START SLAVE;'
 docker exec mysql_slave mysql -uroot -ppwd -e 'SHOW SLAVE STATUS \G'
-
-echo "and for second slave"
-
-MS_STATUS=`docker exec mysql_master  mysql -uroot -ppwd -e "SHOW MASTER STATUS"`
-CURRENT_LOG=`echo $MS_STATUS | awk '{print $6}'`
-CURRENT_POS=`echo $MS_STATUS | awk '{print $7}'`
-docker exec mysql_slave2 mysql -uroot -ppwd -e "INSTALL PLUGIN rpl_semi_sync_slave SONAME 'semisync_slave.so';"
-start_slave2_stmt="CHANGE MASTER TO MASTER_HOST='mysql_master',MASTER_USER='slave_user',MASTER_PASSWORD='slave_pwd', MASTER_LOG_FILE='$CURRENT_LOG',MASTER_LOG_POS=$CURRENT_POS; set global GTID_EXECUTED==$GTID;"
-
-docker exec mysql_slave2 mysql -uroot -ppwd -e "$start_slave2_stmt"
-docker exec mysql_slave2 mysql -uroot -ppwd -e 'START SLAVE;'
-docker exec mysql_slave2 mysql -uroot -ppwd -e 'SHOW SLAVE STATUS \G'
+#
+#echo "and for second slave"
+#
+#MS_STATUS=`docker exec mysql_master  mysql -uroot -ppwd -e "SHOW MASTER STATUS"`
+#CURRENT_LOG=`echo $MS_STATUS | awk '{print $6}'`
+#CURRENT_POS=`echo $MS_STATUS | awk '{print $7}'`
+#docker exec mysql_slave2 mysql -uroot -ppwd -e "INSTALL PLUGIN rpl_semi_sync_slave SONAME 'semisync_slave.so';"
+#start_slave2_stmt="CHANGE MASTER TO MASTER_HOST='mysql_master',MASTER_USER='slave_user',MASTER_PASSWORD='slave_pwd', MASTER_LOG_FILE='$CURRENT_LOG',MASTER_LOG_POS=$CURRENT_POS; set global GTID_EXECUTED==$GTID;"
+#
+#docker exec mysql_slave2 mysql -uroot -ppwd -e "$start_slave2_stmt"
+#docker exec mysql_slave2 mysql -uroot -ppwd -e 'START SLAVE;'
+#docker exec mysql_slave2 mysql -uroot -ppwd -e 'SHOW SLAVE STATUS \G'
 
 #CHANGE MASTER TO MASTER_HOST='mysql_master',MASTER_USER='slave_user',MASTER_PASSWORD='slave_pwd',MASTER_LOG_FILE='mysql-bin.000003',MASTER_LOG_POS=635; START SLAVE;
 #CHANGE MASTER TO MASTER_HOST='mysql_slave2',MASTER_USER='slave_user2',MASTER_PASSWORD='slave_pwd',MASTER_LOG_FILE='mysql-bin.000001',MASTER_LOG_POS=154; START SLAVE;
